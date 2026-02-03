3f96ba95ebdee23e7579756d13039c086b6cd6ae
Tobias Heinen <tobias.heinen@bioptimus.com>
2026-02-03 17:21:44 +0100

    comments

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -116,7 +116,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('FileType', {
   pattern = '*',
   callback = function()
-    vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' })
+    vim.opt_local.formatoptions:remove({ 'r', 'o' })
   end,
 })
 
